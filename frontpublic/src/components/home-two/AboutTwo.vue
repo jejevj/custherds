4504aa419673539a24a02723c314fc5efe437945
J Angga Wijaya <103455042+jejevj@users.noreply.github.com>
2026-06-21 02:24:12 +0700

    feat: update AboutTwo with Affiliate Partners content from custherds.com

--- frontpublic/src/components/home-two/AboutTwo.vue
+++ frontpublic/src/components/home-two/AboutTwo.vue
@@ -9,39 +9,36 @@
                 <div class="col-xl-6 wow fadeInLeft" data-wow-delay="100ms">
                     <div class="about-two__left">
                         <div class="section-title text-left sec-title-animation animation-style2">
-                            <h2 class="section-title__title title-animation">Simple Minimalist For Your Home
-                                Interior</h2>
+                            <h2 class="section-title__title title-animation">Affiliate Partners</h2>
                         </div>
-                        <p class="about-two__text">Inoterior design consultancy firm that brings sensitivity to the
-                            design top restaurants,<br> hotels, offices & homes around the world. We stand for
-                            quality,
+                        <p class="about-two__text">
+                            Bring your networks to the best experiences — luxury villas, beach clubs,
+                            restaurants, spas, activities and earn commission effortlessly.
                         </p>
                         <ul class="list-unstyled about-two__history">
                             <li>
                                 <div class="about-two__history-single">
-                                    <h3 class="about-two__history-year">1997</h3>
-                                    <p class="about-two__history-text">We are committed to the impeccable
-                                        craftsmanship and luxury is reflected in</p>
+                                    <h3 class="about-two__history-year"><span class="icon-right"></span></h3>
+                                    <p class="about-two__history-text">No need to collect cash &amp; transparent commission rate</p>
                                 </div>
                             </li>
                             <li>
                                 <div class="about-two__history-single">
-                                    <h3 class="about-two__history-year">2027</h3>
-                                    <p class="about-two__history-text">We are committed to the impeccable
-                                        craftsmanship and luxury is reflected in</p>
+                                    <h3 class="about-two__history-year"><span class="icon-right"></span></h3>
+                                    <p class="about-two__history-text">Build long-term business partners with monthly payout &amp; legal protection</p>
                                 </div>
                             </li>
                         </ul>
                         <div class="about-two__btn-box">
-                            <router-link to="/about" class="thm-btn about-two__btn">More Details <span
-                                    class="icon-up-right-arrow"></span> </router-link>
+                            <router-link to="/contact" class="thm-btn about-two__btn">Start Earning Today! <span
+                                    class="icon-up-right-arrow"></span></router-link>
                         </div>
                     </div>
                 </div>
                 <div class="col-xl-6 wow fadeInRight" data-wow-delay="300ms">
                     <div class="about-two__right">
                         <div class="about-two__img">
-                            <img src="@/assets/images/resources/about-two-img-1.jpg" alt="">
+                            <img src="https://www.custherds.com/assets/images/slide/page1-2.webp?v=44806c3050915c018d83a707fe577223" alt="Affiliate Partners">
                         </div>
                     </div>
                 </div>
