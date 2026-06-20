1d7b27de0dc04250b5568451aaa2aff608c01bd3
J Angga Wijaya <103455042+jejevj@users.noreply.github.com>
2026-06-21 03:37:46 +0700

    fix: remove unused search & nav-sidebar-icon block from HeaderTwo

--- frontpublic/src/components/layout/header/HeaderTwo.vue
+++ frontpublic/src/components/layout/header/HeaderTwo.vue
@@ -18,15 +18,6 @@
                                 <a href="#" class="mobile-nav__toggler"><i class="fa fa-bars"></i></a>
                                 <NavLinks />
                             </div>
-                            <div class="main-menu-two__search-and-nav-sidebar-icon">
-                                <a href="#" class="main-menu-two__search search-toggler"><span
-                                        class="icon-search-interface-symbol"></span></a>
-                                <div class="main-menu-two__nav-sidebar-icon">
-                                    <a class="navSidebar-button" href="#">
-                                        <span class="icon-text"></span>
-                                    </a>
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
@@ -45,15 +36,6 @@
                                 <div class="main-menu-two__right">
                                     <div class="main-menu-two__main-menu-box">
                                         <NavLinks />
-                                    </div>
-                                    <div class="main-menu-two__search-and-nav-sidebar-icon">
-                                        <a href="#" class="main-menu-two__search search-toggler"><span
-                                                class="icon-search-interface-symbol"></span></a>
-                                        <div class="main-menu-two__nav-sidebar-icon">
-                                            <a class="navSidebar-button" href="#">
-                                                <span class="icon-text"></span>
-                                            </a>
-                                        </div>
                                     </div>
                                 </div>
                             </div>
